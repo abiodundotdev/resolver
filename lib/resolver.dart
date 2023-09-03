f83d6b7156c010f938582a9db0906131fff7814e
Qazeem Abiodun <qudusqazeem@gmail.com>
2023-09-03 22:07:29 +0100

    Initial resolver code commit

--- lib/resolver.dart
+++ lib/resolver.dart
@@ -1,7 +1,77 @@
 library resolver;
 
-/// A Calculator.
-class Calculator {
-  /// Returns [value] plus 1.
-  int addOne(int value) => value + 1;
+class Resolver extends ResolverRegistering {
+  static Map<String, dynamic> registeredDependencies = {};
+  void register<T>(
+    FactoryRegistrant registrant, {
+    String name = "",
+    bool overide = false,
+  }) {
+    final _identity = identifier(T);
+    final factory = registrant.call();
+
+    return;
+  }
+
+  String identifier<T>(T type, [String name = ""]) {
+    if (name.isNotEmpty) {
+      return name;
+    }
+    return "${type.runtimeType}";
+  }
+
+  T get<T>([String name = ""]) {
+    if (name.isNotEmpty) {
+      return registeredDependencies[name];
+    }
+    final _identity = identifier(T);
+    return registeredDependencies[_identity];
+  }
+}
+
+enum Scope { singleton, provider }
+
+abstract class Factory<T> {
+  T? instance;
+}
+
+class SingletonFactory<T> implements Factory<T> {
+  @override
+  T? instance;
+  SingletonFactory(T inst) {
+    instance = inst;
+  }
+}
+
+class ProviderFactory<T> implements Factory<T> {
+  @override
+  T? instance;
+  ProviderFactory(T inst) {
+    instance = inst;
+  }
+}
+
+typedef FactoryRegistrant = Factory Function();
+
+class ResolverRegistering {
+  void registerAll() {}
+}
+
+extension XResolver on Resolver {
+  void registerAll() {
+    register<A>(() => SingletonFactory(A()));
+    register<B>(() => ProviderFactory(B()));
+  }
+}
+
+class A {
+  void printname() {
+    print("My name is Qazeem Abiodun");
+  }
+}
+
+class B {
+  void printname() {
+    print("My name is Qazeem Adelani");
+  }
 }
